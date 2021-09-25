4a00b4dc57e6133d8bd45ef3204b027c4e1b6367
Andika Kurnia <andikakurnia077@gmail.com>
2021-09-25 14:39:27 +0700

    Update CRUD Warehouse

--- src/app/warehouse-details/warehouse-details.component.ts
+++ src/app/warehouse-details/warehouse-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Warehouse } from '../warehouse';
+import { WarehouseService } from '../warehouse.service';
 
 @Component({
   selector: 'app-warehouse-details',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WarehouseDetailsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+	id: String;
+	warehouse: Warehouse;
+  
+	constructor(private route: ActivatedRoute, private router: Router, private warehouseService : WarehouseService) { }
+  
+	backToTheListWarehouse() {
+	  this.router.navigate(['warehouse']);
+	}
+  
+	ngOnInit() {
+  
+	  this.warehouse = new Warehouse();
+  
+	  this.id = this.route.snapshot.params['id'];
+  
+	  this.warehouseService.findWarehouse(this.id).subscribe(
+	  
+		data=>{
+		  console.log(data);
+		  this.warehouse = data;
+		}, error=>console.log(error)
+	  );
+	}
+	}
--- src/app/warehouse-update/warehouse-update.component.ts
+++ src/app/warehouse-update/warehouse-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Warehouse } from '../warehouse';
+import { WarehouseService } from '../warehouse.service';
 
 @Component({
   selector: 'app-warehouse-update',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WarehouseUpdateComponent implements OnInit {
 
-  constructor() { }
+  id: String;
+  warehouse: Warehouse;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private warehouseService: WarehouseService
+  ) {}
 
   ngOnInit() {
+    this.warehouse = new Warehouse();
+
+    this.id = this.route.snapshot.params["id"];
+
+    this.warehouseService.findWarehouse(this.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.warehouse = data;
+      },
+      (error) => console.log(error)
+    );
   }
 
+  updateWarehouse() {
+    this.warehouseService.findWarehouse(this.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.backToTheListWarehouse();
+      },
+      (error) => console.log(error)
+    );
+  }
+  onSubmit() {
+    this.updateWarehouse();
+  }
+
+  backToTheListWarehouse() {
+    this.router.navigate(["warehouse"]);
+  }
 }
